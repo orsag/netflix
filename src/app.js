edb672aa536dcf2efb8d7a3242583c670fcde41b
martin.orsag <martin.orsag@sonalake.com>
2021-03-13 19:55:07 +0100

    Router auth

--- src/app.js
+++ src/app.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
+import { IsUserRedirect } from './helpers/routes';
 import { Home, Browse, SignIn, SignUp } from './pages';
 
 export default function App() {
+  const user = {};
+
   return (
     <Router>
+      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN} exact>
+        <SignIn />
+      </IsUserRedirect>
+      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP} exact>
+        <SignUp />
+      </IsUserRedirect>
       <Route exact path={ROUTES.HOME}>
         <Home />
       </Route>
       <Route exact path={ROUTES.BROWSE}>
         <Browse />
-      </Route>
-      <Route exact path={ROUTES.SIGN_UP}>
-        <SignUp />
-      </Route>
-      <Route exact path={ROUTES.SIGN_IN}>
-        <SignIn />
       </Route>
     </Router>
   );
