d2518955a05f4162cf105cc038b97c8fffb8c785
martin.orsag <martin.orsag@sonalake.com>
2021-03-14 12:44:16 +0100

    Selection filter

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { useContent } from '../hooks';
+import selectionFilter from '../utils';
 
 export default function Browse() {
   const { series } = useContent('series');
-  console.log(series);
+  const { films } = useContent('films');
+  const slides = selectionFilter({ series, films });
 
   return <p>Browse</p>;
 }
