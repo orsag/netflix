dfb2fff5b544c3f3162e332ffb5c216b53180bd7
martin.orsag <martin.orsag@sonalake.com>
2021-03-14 12:36:11 +0100

    UseContent hook

--- src/app.js
+++ src/app.js
@@ -3,9 +3,10 @@ import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
 import { Home, Browse, SignIn, SignUp } from './pages';
+import { useAuthListener } from './hooks';
 
 export default function App() {
-  const user = null;
+  const { user } = useAuthListener();
 
   return (
     <Router>
@@ -19,7 +20,7 @@ export default function App() {
         <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
           <Browse />
         </ProtectedRoute>
-        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME} exact>
           <Home />
         </IsUserRedirect>
       </Switch>
