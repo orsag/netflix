7a014b2e3ac7a40602ae729484eae4fd779dbcb7
martin.orsag <martin.orsag@sonalake.com>
2021-03-15 20:44:41 +0100

    Profiles container

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useContent } from '../hooks';
-import selectionFilter from '../utils';
+import selectionFilter from '../utils/selection-filter';
+import { BrowseContainer } from '../containers/browse';
 
 export default function Browse() {
   const { series } = useContent('series');
   const { films } = useContent('films');
   const slides = selectionFilter({ series, films });
 
-  return <p>Browse</p>;
+  return <BrowseContainer slides={slides} />;
 }
