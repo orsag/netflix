796b33b363810c4d735ff7395bf50cf3d8dee25a
martin.orsag <martin.orsag@sonalake.com>
2021-03-16 09:39:01 +0100

    Loading and Lock body

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,11 +1,28 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { SelectProfileContainer } from './profiles';
 import { FirebaseContext } from '../context/firebase';
+import { Loading, Header } from '../components';
 
 export function BrowseContainer({ slides }) {
   const [profile, setProfile] = useState({});
+  const [loading, setLoading] = useState(true);
   const { firebase } = useContext(FirebaseContext);
   const user = firebase.auth().currentUser || {};
 
-  return <SelectProfileContainer user={user} setProfile={setProfile} />;
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [profile.displayName]);
+
+  // eslint-disable-next-line no-nested-ternary
+  return profile.displayName ? (
+    <>
+      {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+
+      <Header src="joker1" />
+    </>
+  ) : (
+    <SelectProfileContainer user={user} setProfile={setProfile} />
+  );
 }
