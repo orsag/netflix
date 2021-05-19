9ab20438452830fde5302518a038df615ae7be7d
Martin Orsag <martin.orsag108@gmail.com>
2021-05-19 11:24:26 +0200

    Card component

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -40,7 +40,7 @@ Accordion.Header = function AccordionHeader({ children, ...restProps }) {
 };
 
 Accordion.Body = function AccordionBody({ children, ...restProps }) {
-  const { toggleShow, setToggleShow } = useContext(ToggleContext);
+  const { toggleShow } = useContext(ToggleContext);
 
   return toggleShow ? <Body {...restProps}>{children}</Body> : null;
 };
